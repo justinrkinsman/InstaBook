33ccc44f4e9162dfecae32a36cfd9a46ae4eddae
Justin <justinrkinsman@gmail.com>
2023-01-16 10:56:34 -0400

    Add routes to catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,7 +4,6 @@ const fetch = (...args) =>
   import('node-fetch').then(({default: fetch}) => fetch(...args))
 const { DateTime } = require('luxon')
 const Post = require('../models/post')
-const User = require('../models/user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,26 +11,66 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/homepage', function(req, res, next) {
+  if (!req.user) {
+    res.redirect('/login')
+  }
+  /*const requestUrl = `http://localhost:3000/api/homepage`
+  fetch(requestUrl)
+  .then(response => response.json())
+  .then(data => {
     if (!req.user) {
-      res.redirect('/login')
+      return res.render('index.pug', { title: "InstaBook", posts: data, user: null })
+    }else{
+      return res.render('index.pug', { title: "InstaBook", posts: data, user: req.user.username });
     }
-    /*const requestUrl = `http://localhost:3000/api/homepage`
-    fetch(requestUrl)
-    .then(response => response.json())
-    .then(data => {
-        if (!req.user) {
-            return res.render('index.pug', { title: "InstaBook", posts: data, user: null })
-        }else{
-            return res.render('index.pug', { title: "InstaBook", posts: data, user: req.user.first_name})
-        }
-      })*/
-      //return res.redirect('/login')
-    res.render('index.pug', {title: "InstaBook", user: req.user.first_name})
-  });
-  
-  /* GET login page. */
-  router.get('/login', function(req, res, next) {
-    res.render('login.pug', {title: "Login"})
-  })
+  })*/
+  res.render('index.pug', {title: "InstaBook", user: req.user.first_name})
+});
 
-module.exports = router
+/* GET new post page */
+router.get('/new-post', function(req, res, next) {
+  res.render('new-post.pug', {title: "New Post"})
+})
+
+/* POST new post */
+router.post('/new-post', function(req, res, next) {
+  const date = new Date()
+  const newTimestamp = DateTime.fromJSDate(date).toFormat("MMMM d yyyy h:mm a")
+  postDetail = {
+    body: req.body.body,
+    author: req.body.author,
+    comments: [],
+    likes: 0,
+    timestamp: newTimestamp,
+    db_timestamp: date
+  }
+
+  let post = new Post(postDetail)
+
+  post.save(function (err) {
+    return
+  })
+  res.redirect('/')
+})
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login.pug', {title: "Login"})
+})
+
+/* GET sign up page */
+router.get('/signup', function(req, res, next) {
+  res.render('signup.pug', {title: "Sign Up"})
+})
+
+/* GET failed login page */
+router.get('/failed-login', function(req, res, next) {
+  res.render('failure.pug', {title: 'Login Attempt Failed'})
+})
+
+
+
+/* POST send friend request*/
+
+
+module.exports = router;
