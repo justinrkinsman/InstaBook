7c59483ba33ee75cd7a43923c1955725b915a73f
Justin <justinrkinsman@gmail.com>
2023-03-30 13:35:49 -0300

    Create new schema for notifications

--- models/notifications.js
+++ models/notifications.js
@@ -1,10 +1,21 @@
-/*const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
 
 const Schema = mongoose.Schema
 
 const NotificationSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
-    accepted_friend_requests: {}
+    post: { type: Schema.Types.ObjectId, ref: 'Post' },
+    accepted_friend_requests: { type: Schema.Types.ObjectId, ref: 'User' },
+    received_friend_requests: { type: Schema.Types.ObjectId, ref: 'User' },
+    likes: { type: String },
+    comments: { type: String },
+    timestamp: { type: String, required: true },
+    db_timestamp: { type: Date, required: true },
 })
 
-module.exports = mongoose.model("Notification", NotificationSchema)*/
+NotificationSchema.virtual("formatted_timestamp").get(function () {
+    return DateTime.formJSDate(this.timestamp).toFormat("MMMM d yyyy", " h:mm a")
+})
+
+module.exports = mongoose.model("Notification", NotificationSchema)
